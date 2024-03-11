869a0837074aa83e22fbde9ee934775be203040e
kevin-pietruszka <kevinpiet13@gmail.com>
2024-03-11 19:47:55 -0400

    app and route setup

---
+++ src/app.rs
@@ -0,0 +1,65 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+
+#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+pub enum Route {
+    #[at("/projects/:id")]
+    Project { id: u64 },
+    #[at("/projects")]
+    Projects,
+    #[at("/skills/:id")]
+    Skill { id: u64 },
+    #[at("/skills")]
+    Skills,
+    #[at("/experience/:id")]
+    Experience { id: u64 },
+    #[at("/experience")]
+    Experiences,
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+
+fn switch(route: Route) {
+    match route {
+        Route::Project { id } => {
+            println!("Navigating to Project with ID: {}", id);
+        },
+        Route::Projects => {
+            println!("Navigating to Projects List");
+        },
+        Route::Skill { id } => {
+            println!("Navigating to Skill with ID: {}", id);
+        },
+        Route::Skills => {
+            println!("Navigating to Skills List");
+        },
+        Route::Experience { id } => {
+            println!("Navigating to Experience with ID: {}", id);
+        },
+        Route::Experiences => {
+            println!("Navigating to Experiences List");
+        },
+        Route::Home => {
+            println!("Navigating to Home");
+        },
+        Route::NotFound => {
+            println!("Page Not Found (404)");
+        },
+    }
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main>
+            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+            <h1>{ "Hello World!" }</h1>
+            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+        </main>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod app;
+
+use app::App;
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
