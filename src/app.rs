8f1684a786bfaf35694e5bb61095cd8c9b668d55
kevin-pietruszka <kevinpiet13@gmail.com>
2024-04-23 21:34:23 -0400

    setup navbar and placeholders for routes

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+use yew::html::Scope;
 
 #[derive(Routable, PartialEq, Eq, Clone, Debug)]
 pub enum Route {
@@ -8,8 +9,6 @@ pub enum Route {
     Project { id: u64 },
     #[at("/projects")]
     Projects,
-    #[at("/skills")]
-    Skills,
     #[at("/experience/:id")]
     Experience { id: u64 },
     #[at("/experience")]
@@ -22,39 +21,114 @@ pub enum Route {
 }
 
 
-fn switch(route: Route) {
+fn switch(route: Route) -> Html {
     match route {
         Route::Project { id } => {
-            println!("Navigating to Project with ID: {}", id);
+            html!{<> </>}
         },
         Route::Projects => {
-            println!("Navigating to Projects List");
-        },
-        Route::Skills => {
-            println!("Navigating to Skills List");
+            html!{<> </>}
         },
         Route::Experience { id } => {
-            println!("Navigating to Experience with ID: {}", id);
+            html!{<> </>}
         },
         Route::Experiences => {
-            println!("Navigating to Experiences List");
+            html!{<> </>}
         },
         Route::Home => {
-            println!("Navigating to Home");
+            html!{<> </>}
         },
         Route::NotFound => {
-            println!("Page Not Found (404)");
+            html!{<> </>}
         },
     }
 }
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+pub enum Msg {
+    ToggleNavbar,
+}
+pub struct App {
+    navbar_active: bool,
+}
+
+impl App {
+    fn view_nav(&self, link: &Scope<Self>) -> Html {
+
+        let Self { navbar_active, .. } = *self;
+
+        let active_class = if !navbar_active { "is-active" } else { "" };
+
+        html! {
+            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <h1 class="navbar-item is-size-3">{ "Portfolio" }</h1>
+
+                    <button class={classes!("navbar-burger", "burger", active_class)}
+                        aria-label="menu" aria-expanded="false"
+                        onclick={link.callback(|_| Msg::ToggleNavbar)}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class={classes!("navbar-menu", active_class)}>
+                    <div class="navbar-start">
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                            { "Home" }
+                        </Link<Route>>
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Experiences}>
+                            { "Experience" }
+                        </Link<Route>>
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Projects}>
+                            { "Projects" }
+                        </Link<Route>>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            navbar_active: false,
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+
+        match msg {
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
+                true
+            },
+            _ => unreachable!()
+        }
+        
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <BrowserRouter>
+                { self.view_nav(ctx.link()) }
+
+                <main>
+                    <Switch<Route> render={switch} />
+                </main>
+                <footer class="footer">
+                    <div class="content has-text-centered">
+                        { "Powered by " }
+                        <a href="https://yew.rs">{ "Yew" }</a>
+                        { " using " }
+                        <a href="https://bulma.io">{ "Bulma" }</a>
+                    </div>
+                </footer>
+            </BrowserRouter>
+        } 
     }
 }
