6c3ea277b8ff9929168700cb23e4ffcdce98df9c
kevin-pietruszka <kevinpiet13@gmail.com>
2024-05-01 12:36:39 -0400

    hide unused parameter

--- src/pages/page_not_found.rs
+++ src/pages/page_not_found.rs
@@ -7,11 +7,11 @@ impl Component for PageNotFound {
     type Message = ();
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Self
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <section class="hero is-danger is-bold is-large">
                 <div class="hero-body">
